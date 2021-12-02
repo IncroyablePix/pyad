0a815166c73a78e01a46d2c35bd145e98c2a5bb3
root <root@localhost.localdomain>
2021-12-02 23:14:35 +0100

    RSync & SCP command generators and executors

---
+++ save_cron/net_save.py
@@ -0,0 +1,44 @@
+import os
+import sys
+import subprocess
+
+class SCPManager:
+	def __init__(self, dest_addr: str = "", dest_user: str = ""):
+		self.dest_addr = dest_addr
+		self.dest_user = dest_user
+
+
+	def send_cmd(self, to_copy: str = None, dest_path: str = None):
+		return f"""/usr/bin/scp {to_copy} {self.dest_user}@{self.dest_addr}:{dest_path}"""
+
+
+	def send(self, to_copy: str = None, dest_path: str = None):
+		result = subprocess.run(self.send_cmd(to_copy = to_copy, dest_path = dest_path), shell = True)
+		return result
+
+
+	def get_cmd(self, to_ask: str = None, dest_path: str = None):
+		destination: str = "./" if dest_path == None else dest_path
+		return f"""/usr/bin/scp {self.dest_user}@{self.dest_addr}:{to_ask} {destination}"""
+
+	
+	def ask(self, to_ask: str = None, dest_path: str = None):
+		result = subprocess.run(self.get_cmd(to_ask, dest_path), shell = True)
+		return result
+
+
+class RSyncManager:
+	def __init__(self, dest_addr: str = "", dest_user: str = ""):
+		self.dest_addr = dest_addr
+		self.dest_user = dest_user
+
+
+	def synchronize_cmd(self, to_synchronize: str = "", dest_path: str = ""):
+		return f"""/usr/bin/rsync -avz {to_synchronize} {self.dest_user}@{self.dest_addr}:{dest_path} -e ssh"""
+
+	
+	def synchronize(self, to_synchronize: str = "", dest_path: str = ""):
+		result = subprocess.run(self.synchronize_cmd(to_synchronize = to_synchronize, dest_path = dest_path), shell = True)
+		return result
+
+		
---
+++ save_cron/planned.py
@@ -0,0 +1,6 @@
+import os
+import sys
+import subprocess
+
+class RepeatTask:
+	
