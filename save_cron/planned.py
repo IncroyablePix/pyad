a7b3445ca070f6c1917262093cbe0b4f2c6b2c49
root <root@localhost.localdomain>
2021-12-07 11:52:45 +0100

    at

--- save_cron/planned.py
+++ save_cron/planned.py
@@ -2,6 +2,7 @@ import os
 import sys
 import subprocess
 import datetime
+import uuid
 
 '''TODO: Pour  des  raisons  de  sécurité,  il  est  possible  de  limiter  les  utilisateurs  autorisés  à  exécuter  ces 
 commandes. Par défaut, tous les utilisateurs peuvent programmer des tâches ponctuelles ou 
@@ -22,15 +23,32 @@ class RepeatTask:
 		pass
 
 
-class OntTimeTask:
-	def __init(self):
+class OneTimeTask:
+	dest_dir: str = f"""/var/pyad/jobs"""
+
+	def __init__(self):
 		self.time = ""
+		
+		if not os.path.isdir(OneTimeTask.dest_dir):
+			os.makedirs(OneTimeTask.dest_dir, mode=0o700)
+
+		self.job_file = f"""{uuid.uuid4()}.sh"""
 
 
 	def set_absolute_time(self, time: datetime):
-		self.time = f"""{time.strftime("%a")} {time.strftime("%b")} {time.day:02d} {time.hour:02d}:{time.minute:02d}:{time.second:02d} {time.year}"""
+		self.time = f"""{time.hour:02d}:{time.minute:02d} {time.strftime("%b").lower()} {time.day:02d}  {time.year}"""
 
 	
-	def execute(self, command: str = ""):
-		result = subprocess.run(f"""at {self.time} -f "{command}" """, shell = True)
+	def execute(self, command_list: list = [], keep_job_file: bool = False):
+		dest_file: str = f"""{OneTimeTask.dest_dir}/{self.job_file}"""
+
+		with open(dest_file, "w") as job:
+			for line in command_list:
+				job.write(f"""{line}\n""")
+
+			if not keep_job_file:
+				job.write(f"""/usr/bin/rm {dest_file}\n""")
+
+		cmd: str = f"""/usr/bin/at {self.time} -f "{dest_file}" """
+		result = subprocess.run(cmd, shell = True)
 		return result
