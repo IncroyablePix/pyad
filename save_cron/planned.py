5411604083692acd7413926b7799145e0d486836
root <root@localhost.localdomain>
2021-12-07 12:28:39 +0100

    crontab begin (undone)

--- save_cron/planned.py
+++ save_cron/planned.py
@@ -11,20 +11,63 @@ dans /etc/cron.allow, /etc/cron.deny, /etc/at.allow ou /etc/at.deny. Pour plus
 d’information, référez-vous aux pages de manuel de ces commandes.'''
 
 class RepeatTask:
+	dest_dir: str = f"""/var/pyad/jobs/cron"""
+
 	def __init__(self):
-		pass
+		self.time = ""
+
+		if not os.path.isdir(RepeatTask.dest_dir):
+			os.makedirs(RepeatTask.dest_dir, mode=0o700)
+
+		self.job_file = f"""{uuid.uuid4()}.sh"""
+
+		self.minute = "*"
+		self.hour = "*"
+		self.day = "*"
+		self.month = "*"
+		self.day_of_week = "*"
 
 
-	def set_time(self):
-		pass
+	def set_time(self, minute: str = None, hour: str = None, day: str = None, month: str = None, day_of_week = None):
+		if minute != None:
+			self.minute = minute
+		
+		if hour != None:
+			self.hour = hour
+		
+		if day != None:
+			self.day = day
+		
+		if month != None:
+			self.month = month
+
+		if day_of_week != None:
+			self.day_of_week = day_of_week
 
 
-	def execute(self):
-		pass
+	def execute(self, command_list: list = []):
+		dest_file: str = f"""{RepeatTask.dest_dir}/{self.job_file}"""
+
+		with open(dest_file, "w") as job:
+			job.write(f"""{self.minute} {self.hour} {self.day} {self.month} {self.day_of_week} '""")
+			first: bool = True
+			for line in command_list:
+				if not first:
+					job.write(f""" && """)
+					first = False
+
+				job.write(f"""{line}""")
+
+			job.write(f"""'""")
+
+		cmd: str = f"""/usr/bin/crontab -e {dest_file} """
+		print(cmd)
+		result = subprocess.run(cmd, shell = True)
+		return result
 
 
 class OneTimeTask:
-	dest_dir: str = f"""/var/pyad/jobs"""
+	dest_dir: str = f"""/var/pyad/jobs/at"""
 
 	def __init__(self):
 		self.time = ""
@@ -52,3 +95,4 @@ class OneTimeTask:
 		cmd: str = f"""/usr/bin/at {self.time} -f "{dest_file}" """
 		result = subprocess.run(cmd, shell = True)
 		return result
+
